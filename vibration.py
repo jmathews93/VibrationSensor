b1368df60df2f5f7f9c0db4fb2817beb99c09ecd
Jared <mathrock93@gmail.com>
2021-12-08 22:03:25 -0500

    vibration.py now writes to csv

--- vibration.py
+++ vibration.py
@@ -1,26 +1,35 @@
-#!/usr/bin/python
 import RPi.GPIO as GPIO
 import time
 
 
-#GPIO SETUP
-channel = 17
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(channel, GPIO.IN)
+def main(args):
+    
+	# GPIO SETUP
+	channel = 17
+	GPIO.setmode(GPIO.BCM)
+	GPIO.setup(channel, GPIO.IN)
+	f = open('data.csv', "a")
+	f.write("data, time, deviceNum")
 
-while True:
-	input_value = GPIO.input(channel)
-	print(input_value)
+	while True:
+		try:
+			input_value = GPIO.input(channel)
+			print(input_value, time.time())
+			data_point = "{},{},{}\n".format(input_value, time.time(), args["deviceNum"])
+			f.write(data_point)
+		except KeyboardInterrupt:
+			break
+	f.close()
 
-#def callback(channel):
-#        if GPIO.input(channel):
-#                print("Movement Detected!")
-#        else:
-#                print("Movement Detected!")
 
-#GPIO.add_event_detect(channel, GPIO.BOTH, bouncetime=300)  # let us know when the pin goes HIGH or LOW
-#GPIO.add_event_callback(channel, callback)  # assign function to GPIO PIN, Run function on change
+if __name__ == "__main__":
 
-# infinite loop
-#while True:
-#        time.sleep(1)
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-ip", "--ipaddress", type=str, required=True)
+    parser.add_argument("-f", "--filename", type=str)
+    parser.add_argument("-p", "--port", type=str, default=8080)
+    parser.add_argument("-dn", "--deviceNum", type=int, default=1)
+    args = vars(parser.parse_args())
+    print(args)
+    main(args)
